06b1061b6001c8fd01a182e45e51a0a185325c79
Phelix <dev.phelixochieng@gmail.com>
2025-02-17 00:03:38 +0300

    feat (settings): properly read from settings

--- src-tauri/src/settings/models.rs
+++ src-tauri/src/settings/models.rs
@@ -5,8 +5,9 @@ use tauri::AppHandle;
 
 use crate::utils::get_app_data_dir;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct AppSettings {
+    #[serde(default)]
     pub scan: ScanSettings,
 }
 impl AppSettings {
@@ -14,9 +15,15 @@ impl AppSettings {
     pub fn load(handle: &AppHandle) -> Self {
         let json_path = get_app_data_dir(handle).join("settings.json");
         if let Ok(content) = read_to_string(json_path) {
-            if let Ok(settings) = serde_json::from_str::<AppSettings>(&content) {
-                println!("Loaded settings from file");
-                return settings;
+            println!("Loading settings from file. {content}");
+            match serde_json::from_str::<AppSettings>(&content) {
+                Ok(settings) => {
+                    println!("Loaded settings from file: {:?}", settings);
+                    return settings;
+                }
+                Err(e) => {
+                    eprintln!("An error occured: {:?}", e);
+                }
             };
         }
 
@@ -38,14 +45,24 @@ impl Default for AppSettings {
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ScanSettings {
+    #[serde(default = "ignore_dirs_default")]
     pub ignore_dirs: Vec<String>,
+
+    #[serde(default = "ignore_extensions_default")]
     pub ignore_extensions: Vec<String>,
 }
 impl Default for ScanSettings {
     fn default() -> Self {
         ScanSettings {
-            ignore_dirs: vec![".git".to_string(), "node_modules".to_string()],
-            ignore_extensions: vec!["lock".to_string()],
+            ignore_dirs: ignore_dirs_default(),
+            ignore_extensions: ignore_extensions_default(),
         }
     }
+}
+
+fn ignore_dirs_default() -> Vec<String> {
+    vec![".git".to_string(), "node_modules".to_string()]
+}
+fn ignore_extensions_default() -> Vec<String> {
+    vec!["lock".to_string()]
 }
